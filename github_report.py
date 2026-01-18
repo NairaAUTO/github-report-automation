c43507824bfb4dbd90f01b242789c2448a28c717
NairaAUTO <arielhuff2000@gmail.com>
2026-01-17 23:04:43 -0600

    Update github_report.py

--- github_report.py
+++ github_report.py
@@ -8,38 +8,43 @@ EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_TO = os.environ.get("EMAIL_TO")
 
-
 OWNER = "torvalds"
 REPO = "linux"
 GITHUB_API = "https://api.github.com"
+
+
 def get_repo_info():
     url = f"{GITHUB_API}/repos/{OWNER}/{REPO}"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
+
 def get_commits_last_7_days():
     since = datetime.utcnow() - timedelta(days=7)
     url = f"{GITHUB_API}/repos/{OWNER}/{REPO}/commits"
-
-    params = {
-        "since": since.isoformat() + "Z"
-    }
-
+    params = {"since": since.isoformat() + "Z"}
     response = requests.get(url, params=params)
     response.raise_for_status()
-
     return len(response.json())
+
 
 def generate_report():
     repo = get_repo_info()
     commits = get_commits_last_7_days()
 
-    full_name = repo["full_name"]
-    stars = repo["stargazers_count"]
-    forks = repo["forks_count"]
-    issues = repo["open_issues_count"]
-    generated_time = datetime.now()
+    report = f"""
+GitHub Weekly Report
+
+Repository: {repo['full_name']}
+Stars: {repo['stargazers_count']}
+Forks: {repo['forks_count']}
+Open Issues: {repo['open_issues_count']}
+Commits (Last 7 Days): {commits}
+Generated: {datetime.now()}
+"""
+    return report
+
 
 def send_email_report(report):
     msg = MIMEText(report)
@@ -52,18 +57,6 @@ def send_email_report(report):
         server.send_message(msg)
 
 
-    report = f"""
-GitHub Weekly Report
-Repository: {full_name}
-Stars: {stars}
-Forks: {forks}
-Open Issues: {issues}
-Commits (Last 7 Days): {commits}
-Generated: {generated_time}
-"""
-    return report
-
 if __name__ == "__main__":
     report = generate_report()
-send_email_report(report)
-
+    send_email_report(report)
